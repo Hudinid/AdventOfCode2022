8ec2a92cf92a9d18757d05eac75db0d8655b1981
Hudinid <bjhu4481cobra@gmail.com>
2022-11-30 23:09:20 -0800

    Day1 files

---
+++ Day1/Day1Pt1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Day1Pt1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        long max = 0;
+        
+        long tempSum = 0;
+        
+        for(int i = 0; i < 2243; i ++) { //2243 lines
+            String line = br.readLine();
+            if(line.equals("")) {
+                max = Math.max(max, tempSum);
+                tempSum = 0;
+            }
+            else { 
+                tempSum += Long.parseLong(line);
+            }
+        }
+
+        pw.println(max);
+        pw.close(); 
+    }
+}
---
+++ Day1/Day1Pt2.java
@@ -0,0 +1,32 @@
+package AdventOfCode2022.Day1;
+import java.io.*;
+import java.util.*;
+public class Day1Pt2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+
+        ArrayList<Long> numbers = new ArrayList<>();
+        
+        long tempSum = 0;
+        
+        for(int i = 0; i < 2243; i ++) { //2243 lines
+            String line = br.readLine();
+            if(line.equals("")) {
+                numbers.add(tempSum);
+                tempSum = 0;
+            }
+            else { 
+                tempSum += Long.parseLong(line);
+            }
+        }
+        numbers.add(tempSum);
+        Collections.sort(numbers);
+        // pw.println(numbers.get(numbers.size()-1));
+        // pw.println(numbers.get(numbers.size()-2));
+        // pw.println(numbers.get(numbers.size()-3));
+
+        pw.println(numbers.get(numbers.size()-1) + numbers.get(numbers.size()-2) + numbers.get(numbers.size()-3));
+        pw.close(); 
+    }
+}
